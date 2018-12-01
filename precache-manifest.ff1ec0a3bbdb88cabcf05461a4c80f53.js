a5f4344f43b9a278524838180dfcbdf20789c3e6
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-12-01 21:36:21 +0300

    Updates

---
+++ precache-manifest.ff1ec0a3bbdb88cabcf05461a4c80f53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e30d20cc07c4241755d",
+    "url": "/game15/static/css/main.1aa0128c.chunk.css"
+  },
+  {
+    "revision": "1e30d20cc07c4241755d",
+    "url": "/game15/static/js/main.1e30d20c.chunk.js"
+  },
+  {
+    "revision": "4cd17c70348584811180",
+    "url": "/game15/static/css/1.baa971c3.chunk.css"
+  },
+  {
+    "revision": "4cd17c70348584811180",
+    "url": "/game15/static/js/1.4cd17c70.chunk.js"
+  },
+  {
+    "revision": "f1a4bd4686e56d8e6371",
+    "url": "/game15/static/js/runtime~main.f1a4bd46.js"
+  },
+  {
+    "revision": "f0816eb570871862b69aec8a77150f27",
+    "url": "/game15/static/media/gloria-hallelujah-latin-400.f0816eb5.woff"
+  },
+  {
+    "revision": "403e8e9a007d9700f935b16b9e34bd35",
+    "url": "/game15/static/media/gloria-hallelujah-latin-400.403e8e9a.woff2"
+  },
+  {
+    "revision": "792a2e91df4d17f900432bf65b16f0fd",
+    "url": "/game15/index.html"
+  }
+];
